786fdbe7c50896a3708419c0c67cb4ada13f03c0
Игорь Багдамян <i.bagdamyan365@Snegirsoft.com>
2020-02-20 11:01:10 +0300

    AnalyzersAdded

---
+++ SomethingObviouslyUseless/Analyzers/ByteArrayToArrayPool/ByteArrayToArrayPoolAnalyzer/ByteArrayToArrayPoolAnalyzerAnalyzer.cs
@@ -0,0 +1,43 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Reflection;
+
+namespace ByteArrayToArrayPoolAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ByteArrayToArrayPoolAnalyzerAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "ByteArrayToArrayPoolAnalyzer";
+
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Best practices";
+
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {            
+            context.RegisterOperationAction(AnalyzeArrayCreation, OperationKind.ArrayCreation);
+        }
+
+        public void AnalyzeArrayCreation(OperationAnalysisContext context)
+        {
+            var op = (IArrayCreationOperation)context.Operation;
+
+            if (op.DimensionSizes.Length == 1 && op.DimensionSizes[0].ConstantValue.HasValue // check that our array is const
+                && op.DimensionSizes[0].ConstantValue.Value is int value && value > 10) // not sure if there can be something other than integer after const check
+            {
+                var diagnostic = Diagnostic.Create(Rule, op.Syntax.GetLocation());
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
---
+++ SomethingObviouslyUseless/Analyzers/ByteArrayToArrayPool/ByteArrayToArrayPoolAnalyzer/ByteArrayToArrayPoolAnalyzerCodeFixProvider.cs
@@ -0,0 +1,70 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Operations;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ByteArrayToArrayPoolAnalyzer
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(ByteArrayToArrayPoolAnalyzerCodeFixProvider)), Shared]
+    public class ByteArrayToArrayPoolAnalyzerCodeFixProvider : CodeFixProvider
+    {
+        private const string title = "Use ArrayPool";
+
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+            => ImmutableArray.Create(ByteArrayToArrayPoolAnalyzerAnalyzer.DiagnosticId);
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var diagnostic = context.Diagnostics.First();
+
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: title,
+                    createChangedDocument: c => UseArrayPool(context.Document, diagnostic, c),
+                    equivalenceKey: title),
+                diagnostic);
+        }
+
+        private async Task<Document> UseArrayPool(Document document, Diagnostic diagnostic, CancellationToken cancellationToken)
+        {            
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+            var originalExpression = root.FindNode(diagnostic.Location.SourceSpan);
+            var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
+
+            // array type
+            var operation = (IArrayCreationOperation)semanticModel.GetOperation(originalExpression);
+            var typeSymbol = (IArrayTypeSymbol)operation.Type;
+            var elementType = typeSymbol.ElementType;
+
+            var generator = SyntaxGenerator.GetGenerator(document);
+
+            // ArrayPool<T>.Shared.Rent
+            var arrayPool = generator.GenericName("ArrayPool", elementType);
+            var callShared = generator.MemberAccessExpression(arrayPool, "Shared");
+            var callRent = generator.MemberAccessExpression(callShared, "Rent");
+
+            // const argument
+            // note that we switch to SyntaxFactory here
+            var constantValue = SyntaxFactory.Literal((int)operation.DimensionSizes[0].ConstantValue.Value);
+            var constantExpression = SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression, constantValue);
+            var argumentExpression = SyntaxFactory.Argument(constantExpression);
+
+            // ArrayPool<T>.Shared.Rent(const) invocation
+            var invocationExpression = generator.InvocationExpression(callRent, argumentExpression);
+
+            return document.WithSyntaxRoot(root.ReplaceNode(originalExpression, invocationExpression));
+        }
+    }
+}
