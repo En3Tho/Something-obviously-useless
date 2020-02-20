42f4daba5f6ed70a32c9b5f134735c410fce14d9
Игорь Багдамян <i.bagdamyan365@Snegirsoft.com>
2020-02-20 10:46:56 +0300

    SelfAssignmentAnalyzer

---
+++ SomethingObviouslyUseless/Analyzers/SelfAssignmentAnalyzer/SelfAssignmentAnalyzer/SelfAssignmentAnalyzer.cs
@@ -0,0 +1,67 @@
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace SelfAssignmentAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class SelfAssignmentAnalyzer : DiagnosticAnalyzer
+    {
+        private const string Category = "SelfAssignment";
+
+        private static readonly DiagnosticDescriptor SimpleSelfAssignmentRule = new DiagnosticDescriptor("SACS0001", "Self assignment detected by C#.", "Redundant self assignment detected by C#.", Category, DiagnosticSeverity.Warning, true);
+        private static readonly DiagnosticDescriptor PropertySelfReferenceRule = new DiagnosticDescriptor("SACS0002", "Property self reference detected by C#.", "Property self reference detected by C#. Potentially a bug.", Category, DiagnosticSeverity.Warning, true);
+        private static readonly DiagnosticDescriptor PropertySelfCompoundAssignmentRule = new DiagnosticDescriptor("SACS0003", "Property self compound assignment detected by C#.", "Property self compound assignment detected by C#. Change this to a method", Category, DiagnosticSeverity.Warning, true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
+            ImmutableArray.Create(SimpleSelfAssignmentRule, PropertySelfReferenceRule, PropertySelfCompoundAssignmentRule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterOperationAction(AnalyzeSimpleAssignment, OperationKind.SimpleAssignment);
+            context.RegisterOperationAction(AnalyzeCompoundAssignment, OperationKind.CompoundAssignment);
+            context.RegisterOperationAction(AnalyzePropertyReference, OperationKind.PropertyReference);
+        }
+
+        private static bool IsMatch<T>(IOperation target, IOperation value)
+            => target is T tTarget && value is T tValue && tTarget.Equals(tValue);
+        
+        private static void AnalyzeSimpleAssignment(OperationAnalysisContext context)
+        {
+            var operation = (ISimpleAssignmentOperation)context.Operation;
+
+            if (IsMatch<IParameterReferenceOperation>(operation.Target, operation.Value)
+                || IsMatch<IPropertyReferenceOperation>(operation.Target, operation.Value)
+                || IsMatch<IFieldReferenceOperation>(operation.Target, operation.Value))
+            {
+                var diagnostic = Diagnostic.Create(SimpleSelfAssignmentRule, operation.Syntax.GetLocation());
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private static void AnalyzeCompoundAssignment(OperationAnalysisContext context)
+        {
+            var operation = (ICompoundAssignmentOperation)context.Operation;
+
+            if (IsMatch<IParameterReferenceOperation>(operation.Target, operation.Value))
+            {
+                var diagnostic = Diagnostic.Create(PropertySelfCompoundAssignmentRule, operation.Syntax.GetLocation());
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private static void AnalyzePropertyReference(OperationAnalysisContext context)
+        {
+            var operation = (IPropertyReferenceOperation)context.Operation;
+
+            if (context.ContainingSymbol is IMethodSymbol methodSymbol
+                && methodSymbol.AssociatedSymbol is IPropertySymbol property
+                && operation.Property == property)
+            {
+                var diagnostic = Diagnostic.Create(PropertySelfReferenceRule, operation.Syntax.GetLocation());
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
---
+++ SomethingObviouslyUseless/Analyzers/SelfAssignmentAnalyzerFSharp/SelfAssignmentAnalyzer.fs
@@ -0,0 +1,63 @@
+﻿namespace SelfAssignmentAnalyzerFSharp
+
+open Microsoft.CodeAnalysis.Diagnostics
+open Microsoft.CodeAnalysis
+open System.Collections.Immutable
+open Microsoft.CodeAnalysis.Operations
+
+module DiagnosticCreator =    
+    let createDiagnostic (context : OperationAnalysisContext) (rule : DiagnosticDescriptor) =
+        Diagnostic.Create(rule, context.Operation.Syntax.GetLocation())
+        |> context.ReportDiagnostic
+
+module Matcher =
+    let isMatch<'a when 'a :> IOperation and 'a : equality>  (target : IOperation) (value : IOperation) =
+        match (target, value) with
+        | (:? 'a as typedTarget), (:? 'a as typedValue)
+            when typedTarget = typedValue -> true
+        | _ -> false
+
+module SimpleAssignmentAnalysis =
+    let rule = DiagnosticDescriptor("SAFS0001", "Self assignment detected by F#", "Redundant self assignment detected by F#.", "SelfAssignment F#",
+                                    DiagnosticSeverity.Warning, true, null, null, array.Empty<string>())        
+    let analyze (context : OperationAnalysisContext) =
+        let operation = context.Operation :?> ISimpleAssignmentOperation
+        
+        match operation.Target, operation.Value with
+        | t, v when Matcher.isMatch<IPropertyReferenceOperation> t v ->  DiagnosticCreator.createDiagnostic context rule
+        | t, v when Matcher.isMatch<IParameterReferenceOperation> t v ->  DiagnosticCreator.createDiagnostic context rule
+        | t, v when Matcher.isMatch<IFieldReferenceOperation> t v ->  DiagnosticCreator.createDiagnostic context rule
+        | _ -> ()
+
+module PropertyReferenceAnalysis =
+    let rule = DiagnosticDescriptor("SAFS0002", "Property self reference detected by F#", "Property self reference detected by F#. Potentially a bug.", "SelfAssignment F#",
+                                    DiagnosticSeverity.Warning, true, null, null, array.Empty<string>())    
+    let analyze (context : OperationAnalysisContext) =
+        let operation = context.Operation :?> IPropertyReferenceOperation
+    
+        match context.ContainingSymbol with
+        | (:? IMethodSymbol as methodSymbol)
+            when methodSymbol.AssociatedSymbol = (operation.Property :> ISymbol) -> DiagnosticCreator.createDiagnostic context rule
+        | _ -> ()
+
+module CompoundAssignmentAnalysis =
+    let rule =  DiagnosticDescriptor("SAFS0003", "Property self compound assignment detected by F#", "Property self compound assignment detected by F#. Change this to a method.", "SelfAssignment F#",
+                                     DiagnosticSeverity.Warning, true, null, null, array.Empty<string>());        
+    let analyze (context : OperationAnalysisContext) =
+        let operation = context.Operation :?> ICompoundAssignmentOperation
+    
+        match operation.Target, operation.Value with
+        | t, v when Matcher.isMatch<IPropertyReferenceOperation> t v -> DiagnosticCreator.createDiagnostic context rule              
+        | _ -> ()
+
+[<DiagnosticAnalyzer(LanguageNames.CSharp)>]
+type SelfAssignmentAnalyzerFSharp() as _m =
+    inherit DiagnosticAnalyzer()
+
+    override _m.SupportedDiagnostics =
+               ImmutableArray.Create(SimpleAssignmentAnalysis.rule, CompoundAssignmentAnalysis.rule, PropertyReferenceAnalysis.rule)   
+
+    override _m.Initialize (context : AnalysisContext) =
+        context.RegisterOperationAction(SimpleAssignmentAnalysis.analyze, OperationKind.SimpleAssignment)
+        context.RegisterOperationAction(CompoundAssignmentAnalysis.analyze, OperationKind.CompoundAssignment)
+        context.RegisterOperationAction(PropertyReferenceAnalysis.analyze, OperationKind.PropertyReference)
