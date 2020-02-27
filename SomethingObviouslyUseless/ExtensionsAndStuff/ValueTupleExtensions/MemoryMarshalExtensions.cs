0fd735e88c4738a34408844f3da5e0ecbd1bac12
Игорь Багдамян <i.bagdamyan365@Snegirsoft.com>
2020-02-27 15:22:06 +0300

    + memorymarshalextensions

---
+++ SomethingObviouslyUseless/ExtensionsAndStuff/ValueTupleExtensions/MemoryMarshalExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ExtensionsAndStuff.ValueTupleExtensions
+{
+    public static class MemoryMarshalExtensions
+    {
+        public static Span<T> AsSpan<T>(this (T, T, T, T, T, T, T) tuple)
+            => MemoryMarshal.CreateSpan(ref tuple.Item1, 7);
+        
+        public static Span<T> AsSpan<T>(this (T, T, T, T, T, T) tuple)
+            => MemoryMarshal.CreateSpan(ref tuple.Item1, 6);
+        
+        public static Span<T> AsSpan<T>(this (T, T, T, T, T) tuple)
+            => MemoryMarshal.CreateSpan(ref tuple.Item1, 5);
+        
+        public static Span<T> AsSpan<T>(this (T, T, T, T) tuple)
+            => MemoryMarshal.CreateSpan(ref tuple.Item1,4);
+        
+        public static Span<T> AsSpan<T>(this (T, T, T) tuple)
+            => MemoryMarshal.CreateSpan(ref tuple.Item1, 3);
+        
+        public static Span<T> AsSpan<T>(this (T, T) tuple)
+            => MemoryMarshal.CreateSpan(ref tuple.Item1, 2);
+        
+        public static ReadOnlySpan<T> AsReadOnlySpan<T>(this (T, T, T, T, T, T, T) tuple)
+            => MemoryMarshal.CreateReadOnlySpan(ref tuple.Item1, 7);
+        
+        public static ReadOnlySpan<T> AsReadOnlySpan<T>(this (T, T, T, T, T, T) tuple)
+            => MemoryMarshal.CreateReadOnlySpan(ref tuple.Item1, 6);
+        
+        public static ReadOnlySpan<T> AsReadOnlySpan<T>(this (T, T, T, T, T) tuple)
+            => MemoryMarshal.CreateReadOnlySpan(ref tuple.Item1, 5);
+        
+        public static ReadOnlySpan<T> AsReadOnlySpan<T>(this (T, T, T, T) tuple)
+            => MemoryMarshal.CreateReadOnlySpan(ref tuple.Item1, 4);
+        
+        public static ReadOnlySpan<T> AsReadOnlySpan<T>(this (T, T, T) tuple)
+            => MemoryMarshal.CreateReadOnlySpan(ref tuple.Item1, 3);
+        
+        public static ReadOnlySpan<T> AsReadOnlySpan<T>(this (T, T) tuple)
+            => MemoryMarshal.CreateReadOnlySpan(ref tuple.Item1, 2);
+    }
+}
