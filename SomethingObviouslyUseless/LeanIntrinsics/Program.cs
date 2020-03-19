0ed651c5b87e7bc249e2ca54c6c378e8a4477693
Игорь Багдамян <i.bagdamyan365@Snegirsoft.com>
2020-03-19 17:54:25 +0300

    + LeanIntrinsics -> LearnIntrinsics - ReverseFirstAndLastDigit method from program.main TODO: add methods which are using intrinsics (add, reverse, etc) with commentary

--- SomethingObviouslyUseless/LeanIntrinsics/Program.cs
+++ SomethingObviouslyUseless/LeanIntrinsics/Program.cs
@@ -1,31 +1,12 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 
-namespace LeanIntrinsics
+namespace LearnIntrinsics
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.ReadLine();
-        }
-
-        public static int ReverseFirstAndLastDigit_My(int number)
-        {
-            var mul = (number >> 31) | 1;
-            number *= mul;
-
-            var right = number % 10;
-            var left = number;
-            var pow = 1;
-            while (left > 10)
-            {
-                left /= 10;
-                pow *= 10;
-            }
-
-            number = number + right * pow + left - right - left * pow;
-            return number * mul;
         }
     }
 }
