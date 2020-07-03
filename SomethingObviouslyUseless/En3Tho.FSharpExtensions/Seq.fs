173669694473e317de88f0a51ac3f99d5d36a879
En3Tho <37334640+En3Tho@users.noreply.github.com>
2020-07-03 18:12:38 +0300

    More F# extensions

---
+++ SomethingObviouslyUseless/En3Tho.FSharpExtensions/Seq.fs
@@ -0,0 +1,9 @@
+﻿namespace En3Tho.FSharpExtensions
+open System.Linq
+
+module Seq = // TODO: Seq exts
+    let toResizeArray = Enumerable.ToList
+    let ofType<'a> = Enumerable.OfType<'a>
+    let any = Enumerable.Any
+    let count = Enumerable.Count
+    let inline toLookup (keySelector : 'a -> 'b) (elementSelector : 'a -> 'c) seq = Enumerable.ToLookup(seq, keySelector, elementSelector)
